126515550c438f74a2dffbc16bc2d5c4bbf1f96d
Chinmay S B <cbh.mavim@gmail.com>
2025-12-06 12:24:27 +0530

    Feature: Added Routing and Data Validation using Pydantics

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,18 +1,20 @@
+from typing import List
 from fastapi import APIRouter
-from .schema import EventSchema
-
+from .schema import EventListSchema, EventSchema, EventCreateSchema
 
 router = APIRouter(
     prefix='/api/events',
     tags=["API-Events"]
 )
 
-@router.get("/")
+@router.get("/",response_model=EventListSchema)
 def read_events():
-    return {
-        "items" : [1,2,3]
-    }
+    return EventListSchema.results
 
 @router.get("/{event_id}",response_model=EventSchema)
 def get_event(event_id: int):
     return EventSchema(id = event_id)
+
+@router.post("/",response_model=EventSchema)
+def create_events(payload: EventCreateSchema):
+    return EventSchema(id = 123)
